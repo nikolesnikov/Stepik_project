85087557a69df9c2fc3d0fe2733accd1012ccfe5
nikolesnikov <jose337@yandex.ru>
2025-02-04 15:25:59 +0700

    добавлена правка для инициализации браузера

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    service = Service(executable_path='C:/chromedriver/chromedriver.exe')
+    browser = webdriver.Chrome(service=service, options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
