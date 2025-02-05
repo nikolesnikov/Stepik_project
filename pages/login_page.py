33c9a019e08382324ae511ddfe4ca1d2421e02d7
nikolesnikov <jose337@yandex.ru>
2025-02-05 09:21:53 +0700

    добавлен тест для LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url == , "URL doesn't contain login"
+        assert self.browser.current_url.__contains__('ssd'), "URL doesn't contain login"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
